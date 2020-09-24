afd8d9f02ebb4711e4501a0f1ef274d20b9d11fd
Sonal Dhinoja <sonal091186@gmail.com>
2020-09-24 12:18:17 -0400

    First commit - Added files to Local Master Branch

---
+++ src/test/java/com/testCases/TestCases.java
@@ -0,0 +1,26 @@
+package com.testCases;
+
+import org.testng.annotations.Test;
+
+public class TestCases {
+	
+	@Test
+	public void testcase1() {
+		
+		System.out.println("Test Case 1 is successful");
+		
+	}
+	
+	@Test
+	public void testcases2() {
+		
+		System.out.println("Test Case 2 is successful");
+	}
+
+	@Test
+	public void testcases3() {
+		
+		System.out.println("Test Case 3 is successful");
+	}
+
+}
